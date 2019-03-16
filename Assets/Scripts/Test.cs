27f5aeeb8994e1dec6e99537c78ae0ff802d8994
itsagam <adjoinme@gmail.com>
2019-03-16 10:51:50 +0500

    Figured out async kinks, updated Rider settings

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,36 +1,8 @@
 ﻿using UnityEngine;
 
-// TODO: Figure out to handle async functions
-
 namespace Game
 {
-	public class Test : MonoBehaviour
+	public class Test: MonoBehaviour
 	{
-		public Transform Transform;
-
-		// private void Awake()
-		// {
-		// 	SomeTask();
-		// }
-		//
-		// public void Button()
-		// {
-		// }
-		//
-		// private IEnumerator<int> Call()
-		// {
-		// 	yield break;
-		// }
-		//
-		// public static async UniTaskVoid Void(UniTask task)
-		// {
-		// 	await task;
-		// }
-		//
-		// private async UniTask SomeTask()
-		// {
-		// 	await UniTask.Delay(200);
-		// 	print("Hello");
-		// }
 	}
 }
