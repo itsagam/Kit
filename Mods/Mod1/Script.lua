8106eeb27d8caebf95eabdf4f0be59acb41f3bd0
itsagam <adjoinme@gmail.com>
2020-11-25 11:56:40 +0500

    Deleted extra files

--- Mods/Mod1/Script.lua
+++ Mods/Mod1/Script.lua
@@ -1,12 +1,13 @@
 -- There are a lot of ways of scheduling a function:
--- You can implement awake/start/update/... methods and use Full persistence
--- You can use schedule(type)
--- You can use startCoroutine
--- You can call invoke and invokeRepeating
--- Or you can handle it manually
+-- * You can implement awake/start/update/... methods and use Full persistence
+-- * You can use schedule(type)
+-- * You can use startCoroutine
+-- * You can call invoke and invokeRepeating
+-- * Or you can handle it manually
 
 function update()
-	
+	mousePos = UE.Input.mousePosition
+
 end
 
 inject(typeof(CS.Demos.Modding.Demo), "InjectedReplace",
