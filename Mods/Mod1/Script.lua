a065c52b77220434924dab6c43f58ec200dfc1c3
itsagam <adjoinme@gmail.com>
2020-11-26 21:03:49 +0500

    Cleanup

--- Mods/Mod1/Script.lua
+++ Mods/Mod1/Script.lua
@@ -1,10 +1,3 @@
--- There are a lot of ways of scheduling a function:
--- * You can implement awake/start/update/... methods (Full persistence)
--- * You can use schedule(type) (Full persistence)
--- * You can use startCoroutine  (Simple/Full persistence)
--- * You can call invoke and invokeRepeating  (Simple/Full persistence)
--- * Or you can handle it manually
-
 buttonText = UE.GameObject.Find("Canvas/Buttons/ClickMe/Text"):GetComponent(typeof(UE.UI.Text))
 eventSystem = UE.EventSystems.EventSystem.current
 results = System.Collections.Generic.List(UE.EventSystems.RaycastResult)()
